c9ca37c4ff96c62d67e631765992287a0f4d9531
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-25 11:47:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,9 +79,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+				:yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
      }
-  programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
 end
 
 def changing_alan
